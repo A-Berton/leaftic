45fafd5f14321a8254553be4395e65aee797fced
A.BERTON <bertonaubry.a@gmail.com>
2023-05-23 10:49:17 +0200

    setting project

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+//from package installed "node" : beautiful short import paths
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  //from package installed "node" : beautiful short import paths
+  resolve :{
+    alias: [{find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+})
